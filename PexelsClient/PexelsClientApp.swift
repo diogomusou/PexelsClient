8747bd5e8bef90184e6622a76b31074227573738
Diogo Musou <diogo.musou@clearwaterclinical.com>
2025-10-08 09:58:29 +0900

    add default project files

---
+++ PexelsClient/PexelsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsClientApp.swift
+//  PexelsClient
+//
+//  Created by Diogo Musou on 2025-10-08.
+//
+
+import SwiftUI
+
+@main
+struct PexelsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PexelsClientTests/PexelsClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsClientTests.swift
+//  PexelsClientTests
+//
+//  Created by Diogo Musou on 2025-10-08.
+//
+
+import Testing
+@testable import PexelsClient
+
+struct PexelsClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
