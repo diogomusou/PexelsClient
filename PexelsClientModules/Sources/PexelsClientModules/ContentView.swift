d3f1726f132b7e98a376d65230305d220df4c475
Diogo Musou <diogo.musou@clearwaterclinical.com>
2025-10-08 11:12:32 +0900

    add initial project with modules

---
+++ PexelsClientModules/Sources/PexelsClientModules/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+public struct ContentView: View {
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+
+    public init() {}
+}
+
+#Preview {
+    ContentView()
+}
